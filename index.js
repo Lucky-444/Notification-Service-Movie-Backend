e20712fd048668595c66df847c098fdc8057f874
Lucky <sashankswain6@gmail.com>
2026-01-08 02:46:33 +0530

    Basice Set Up Ready

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.listen(process.env.PORT, async () => {
+  console.log("Notification Service Started");
+
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("SuccessFully Connected to Mongo");
+  } catch (error) {}
+});
